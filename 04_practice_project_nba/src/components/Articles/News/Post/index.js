37beb0d5f24b01f4a0be2d5097448c5ebde62466
Stefan <stefan.coros@it-st.ro>
2018-06-08 18:00:59 +0300

    route update

---
+++ 04_practice_project_nba/src/components/Articles/News/Post/index.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import {URL} from '../../../../config';
+
+import styles from '../../articles.css';
+
+class NewsArticles extends Component {
+    render(){
+        return(
+            <div>
+                article view
+            </div>
+        )
+    }
+}
+
+export default NewsArticles;
